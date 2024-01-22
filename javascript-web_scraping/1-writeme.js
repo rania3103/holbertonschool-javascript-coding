0c62c1635aa2775e7fd59172292e684f67af9381
rania3103 <raniaabassi33@gmail.com>
2024-01-22 18:27:03 +0100

    writing in file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const filePath = process.argv[2];
+const content = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(filePath, content, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+  }
+});
